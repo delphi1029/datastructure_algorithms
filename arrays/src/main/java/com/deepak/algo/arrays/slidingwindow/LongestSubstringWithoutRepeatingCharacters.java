8f8e026436d8fd92b23408751e843cc5ea72de0e
Deepak Lamba <lamba.deepak15@gmail.com>
2020-08-02 13:51:05 +0530

    Longest Substring Without Repeating Characters

---
+++ arrays/src/main/java/com/deepak/algo/arrays/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,56 @@
+package com.deepak.algo.arrays.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	
+	
+	public int lengthOfLongestSubstring(String s) {
+		
+		int start = 0;
+	    int end = 0;
+	    Map<Character,Integer> map = new HashMap<>();
+	    int max = 0;
+	    
+	    while(end < s.length()) {
+
+	          char c = s.charAt(end);
+
+	          if(map.containsKey(c)) {
+	            start = Math.max(map.get(c)+1,start);
+	          } 
+	          
+	          map.put(c,end);
+	          max = Math.max(max,end-start+1);
+	          end++;
+	      }
+
+	    return max;
+	    
+	}
+	
+	public int lengthOfLongestSubstring_1(String s) {
+        
+        Map<Character,Integer> map = new HashMap<>();
+        
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        
+        for(end = 0; end<s.length(); end++) {
+            char c = s.charAt(end);
+            
+            while(map.containsKey(c)) {
+                char x = s.charAt(start);
+                start = map.get(x)+1;
+                map.remove(x);
+            } 
+            map.put(c, end);
+            max = Math.max(max,end-start+1);
+        }
+        
+        return max;
+    }
+
+}
